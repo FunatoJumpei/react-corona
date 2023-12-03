b5e6023faf2077894619f566fac13fc15f3165fc
Jumpei Funato <funato.jumpei@tis.co.jp>
2023-12-03 18:48:57 +0900

    APIから送り返され計算されたデータをcountryDataに入れるところまで実装

--- src/App.js
+++ src/App.js
@@ -17,11 +17,20 @@ function App() {
   const getCountryData = () => {
       fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
       .then(res=>res.json())
-      .then(data=>console.log(data[length-1]))
+      .then(data=>{
+        setCountryData({
+          date: data[data.length-1].Date,
+          newConfirmed: data[data.length-1].Confirmed - data[data.length-2].Confirmed,
+          totalConfirmed: data[data.length-1].Confirmed,
+          newRecovered: data[data.length-1].Recovered - data[data.length-2].Recovered,
+          totalRecovered: data[data.length-1].Recovered
+        });
+      })
   }
 
   return (
     <div>  {/*App.cssとの連携は削除*/}
+      {console.log(countryData)}
       <TopPage countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData}/>
     </div>
   );
